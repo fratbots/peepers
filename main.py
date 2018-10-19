a65bd78bc090bf67ee4870cb03d65be1de822ad5
Dmitry Khasanov <dkhasanov@avito.ru>
2018-10-19 16:19:36 +0300

    FPS meter added.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import numpy as np
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
+propFps = cap.get(cv2.CAP_PROP_FPS)
+print("CAP_PROP_FPS: {0}".format(propFps))
+
+realFps = 0
+frameNum = 0
+timeStart = time.time()
+     
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -14,6 +22,13 @@ while(True):
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    frameNum = frameNum + 1
+    timeNow = time.time()
+    realFps = frameNum / (timeNow - timeStart)
+
+    if frameNum % 30 == 0:
+        print("FPS: {0}".format(realFps))
 
 # When everything done, release the capture
 cap.release()
