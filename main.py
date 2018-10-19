70ef45a1475b20f2cf2929e9092146e5c9508014
Qinabu <qinabu@gmail.com>
2018-10-19 17:19:56 +0300

    predictor

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ print("CAP_PROP_FPS: {0}".format(propFps))
 realFps = 0
 frameNum = 0
 timeStart = time.time()
-     
-while(True):
+
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    frame = ret
+
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
