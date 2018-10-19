09ed72acc050ac32cdbcfb3c82cd60883178bbc1
Dmitry Khasanov <dkhasanov@avito.ru>
2018-10-19 19:32:08 +0300

    Basic blink recognition added.

--- main.py
+++ main.py
@@ -1,25 +1,117 @@
 import numpy as np
 import cv2
 import time
+import imutils
+import dlib
+from scipy.spatial import distance as dist
+from imutils import face_utils
 
+# eye aspect ratio to indicate blink
+EYE_AR_THRESH = 0.3
+
+# number of consecutive frames the eye must be below the threshold
+EYE_AR_CONSEC_FRAMES = 3
+ 
+# frame counter
+COUNTER = 0
+
+# total number of blinks
+TOTAL = 0
+
+def eye_aspect_ratio(eye):
+    """Computes eye aspect ratio."""
+
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+
+# Main program
 cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# grab the indexes of the facial landmarks for the left and right eye
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
 propFps = cap.get(cv2.CAP_PROP_FPS)
 print("CAP_PROP_FPS: {0}".format(propFps))
-
 realFps = 0
 frameNum = 0
 timeStart = time.time()
      
 while(True):
-    # Capture frame-by-frame
+    # capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
+    frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # loop over the face detections
+    for rect in rects:
+        # determine the facial landmarks for the face region, then
+        # convert the facial landmark (x, y)-coordinates to a NumPy array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # extract the left and right eye coordinates, then use the
+        # coordinates to compute the eye aspect ratio for both eyes
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        # average the eye aspect ratio together for both eyes
+        ear = (leftEAR + rightEAR) / 2.0
+
+        # compute the convex hull for the left and right eye, then
+        # visualize each of the eyes
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        # check to see if the eye aspect ratio is below the blink
+        # threshold, and if so, increment the blink frame counter
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+        # otherwise, the eye aspect ratio is not below the blink
+        # threshold
+        else:
+            # if the eyes were closed for a sufficient number of
+            # then increment the total number of blinks
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                TOTAL += 1
+
+            # reset the eye frame counter
+            COUNTER = 0
+
+        # draw the total number of blinks on the frame along with
+        # the computed eye aspect ratio for the frame
+        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
@@ -28,7 +120,7 @@ while(True):
     realFps = frameNum / (timeNow - timeStart)
 
     if frameNum % 30 == 0:
-        print("FPS: {0}".format(realFps))
+       print("FPS: {0}".format(realFps))
 
 # When everything done, release the capture
 cap.release()
